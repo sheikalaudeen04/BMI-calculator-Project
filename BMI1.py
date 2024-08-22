70e416de100f29ddd74913ced6d998f89369e195
sheikalaudeen04 <sheikalaudeenk04@gmail.com>
2024-08-22 22:46:25 +0530

    This is my python Project - BMI calculator 1

---
+++ BMI1.py
@@ -0,0 +1,7 @@
+
+height = input("Enter your height: ")
+weight = input("Enter your weight: ")
+height=float(height)
+weight=float(weight)
+bmi=(weight//(height**2))
+print("Your BMI measurement is:",int(bmi))
